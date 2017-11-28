4a91555276779cdb4a12a3a2f69bf0ed5686eaf6
Duy DANG <tao.zen.duy@gmail.com>
2017-11-28 16:11:47 +0100

    members in seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Member.create(email: 'duy.dang@thp.fr', password: 'foobar', password_confirmation: 'foobar', pseudo: 'didou')
+Member.create(email: 'kevin.arnoult@thp.fr', password: 'foobar', password_confirmation: 'foobar', pseudo: 'kevain')
+Member.create(email: 'souya.hibou@thp.fr', password: 'foobar', password_confirmation: 'foobar', pseudo: 'souya')
+Member.create(email: 'nicolas.hovart@thp.fr', password: 'foobar', password_confirmation: 'foobar', pseudo: 'nicou')
+Member.create(email: 'alexandra.auffray@thp.fr', password: 'foobar', password_confirmation: 'foobar', pseudo: 'alex')
+Member.create(email: 'juliette.auvray@thp.fr', password: 'foobar', password_confirmation: 'foobar', pseudo: 'juju')
+Member.create(email: 'naula.gratiant@thp.fr', password: 'foobar', password_confirmation: 'foobar', pseudo: 'naula')
+Member.create(email: 'antoine.antoinou@thp.fr', password: 'foobar', password_confirmation: 'foobar', pseudo: 'toinou')
+Member.create(email: 'brice.labejof@thp.fr', password: 'foobar', password_confirmation: 'foobar', pseudo: 'brice')
