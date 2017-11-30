500a3372e2fb7cd18631427da338ae12be1c84d2
Duy DANG <tao.zen.duy@gmail.com>
2017-11-30 12:35:19 +0100

    modif routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
   resources :events
   devise_for :members
+
   get 'pages/home' , as: 'home'
 
   get 'pages/contact', as: 'contact'
+
+  get 'members', to: 'pages#members', as: 'members'
 
   root 'pages#home'
 
