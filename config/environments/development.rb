8637b14e32af6b2942bddb4de012dc62d2c88a9e
Duy DANG <tao.zen.duy@gmail.com>
2017-12-01 13:02:14 +0100

    install SEND GRID all environments

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -52,19 +52,4 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
-  config.action_mailer.default_url_options = { host: "localhost", port: 3000 }
-
- # Setup the mailer config SENDGRID
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.smtp_settings = {
-  :user_name => 'app82031257@heroku.com',
-  :password => 'jd7k72fw1419',
-  :domain => 'bilobaba.com',
-  :address => 'smtp.sendgrid.net',
-  :port => 587,
-  :authentication => :plain,
-  :enable_starttls_auto => true
-}
-
 end
