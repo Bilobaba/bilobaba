a97997e46116aeca531e37bab1c1d9eda1b76f53
Kévin Arnoult <kevin.arnoult@gmail.com>
2017-12-01 11:38:52 +0100

    emails: try with domain bilobaba instead of gmail (5)

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,31 +89,26 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-
   # Parameter for mail
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default :charset => "utf-8"
 
-  # config.action_mailer.smtp_settings = {
-  # address: "smtp.gmail.com",
-  # port: 587,
   # domain: ENV["GMAIL_DOMAIN"],
-  # authentication: "plain",
-  # enable_starttls_auto: true,
   # user_name: ENV["GMAIL_USERNAME"],
   # password: ENV["GMAIL_PASSWORD"]
 
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
     port: 587,
-    domain: 'bilobaba.herokuapp.com',
+    domain: 'gmail.com',
     authentication: 'plain',
     enable_starttls_auto: true,
     user_name: 'contact.bilobaba@gmail.com',
     password: 'Bilobaba12345'
   }
+
   config.action_mailer.default_url_options = { host: 'bilobaba.herokuapp.com' }
 end
 
