b6fefcf598bac65ac64119bb543455fa7ab96b26
Duy DANG <tao.zen.duy@gmail.com>
2017-12-01 10:24:42 +0100

    test avec parametres directement dans production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,17 +93,26 @@ Rails.application.configure do
   # Parameter for mail
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default :charset => "utf-8"
+
+  # config.action_mailer.smtp_settings = {
+  # address: "smtp.gmail.com",
+  # port: 587,
+  # domain: ENV["GMAIL_DOMAIN"],
+  # authentication: "plain",
+  # enable_starttls_auto: true,
+  # user_name: ENV["GMAIL_USERNAME"],
+  # password: ENV["GMAIL_PASSWORD"]
 
   config.action_mailer.smtp_settings = {
   address: "smtp.gmail.com",
   port: 587,
-  domain: ENV["GMAIL_DOMAIN"],
+  domain: 'gmail.com',
   authentication: "plain",
   enable_starttls_auto: true,
-  user_name: ENV["GMAIL_USERNAME"],
-  password: ENV["GMAIL_PASSWORD"]
+  user_name: 'contact.bilobaba',
+  password: 'Bilobaba12345'
   }
 end
 
