d035ff861d0f1ae07c47a5aa2e8affaa5ca9f428
Duy DANG <tao.zen.duy@gmail.com>
2017-12-01 14:33:32 +0100

    default_url_options

--- config/environment.rb
+++ config/environment.rb
@@ -7,9 +7,11 @@ Rails.application.initialize!
 ActionMailer::Base.smtp_settings = {
   :user_name => ENV['SENDGRID_USERNAME'],
   :password => ENV['SENDGRID_PASSWORD'],
-  :domain => 'yourdomain.com',
+  :domain => 'bilobaba.com',
   :address => 'smtp.sendgrid.net',
   :port => 587,
   :authentication => :plain,
   :enable_starttls_auto => true
 }
+
+Rails.application.routes.default_url_options[:host] = 'Bilobaba'
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -52,4 +52,8 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
+  config.action_controller.default_url_options = {:host => "localhost:3000"}
+
 end
+
+Rails.application.routes.default_url_options[:host] = 'localhost:3000'
