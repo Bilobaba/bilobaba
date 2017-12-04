262c1172c98d8d42f1f8a45e33ba6091bd2748d4
Kévin Arnoult <kevin.arnoult@gmail.com>
2017-12-04 15:33:36 +0100

    Add specific controller for members and add French for devise and simpleform

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,9 @@
 Rails.application.routes.draw do
   resources :events
-  devise_for :members
-
+  devise_for :members, controllers: { sessions: 'members/sessions' }
+  
   get 'pages/home' , as: 'home'
-
   get 'pages/contact', as: 'contact'
 
-  get 'members', to: 'pages#members', as: 'members'
-
   root 'pages#home'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
