1d847d329b50a2fb8e6d12ffcebda5285a131c22
Kévin Arnoult <kevin.arnoult@gmail.com>
2017-12-04 14:28:07 +0100

    Translate date in French

--- config/application.rb
+++ config/application.rb
@@ -14,5 +14,8 @@ module Bilobaba
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
+    config.i18n.default_locale = :fr
   end
 end
