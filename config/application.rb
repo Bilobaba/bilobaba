5178bf6c042fa55b7b81b721c098bcf04cf51637
Kévin Arnoult <kevin.arnoult@gmail.com>
2017-12-07 16:27:14 +0100

    Only display event creation if logged in; use fr translation (from https://github.com/svenfuchs/rails-i18n/blob/master/rails/locale/fr.yml)

--- config/application.rb
+++ config/application.rb
@@ -18,7 +18,7 @@ module Bilobaba
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
-    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :fr
+    config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
+    config.i18n.default_locale = :fr
   end
 end
