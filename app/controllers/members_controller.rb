66aa336f832ade6cf6fbe1e6688a19dfad6d06bc
Kévin Arnoult <kevin.arnoult@gmail.com>
2017-12-14 10:07:14 +0100

    Improve list of professionals; add list of members

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -2,10 +2,17 @@ class MembersController < ApplicationController
   before_action :set_member, only: [:show]
 
   def index
-    @members = Member.with_role :professional
+    if params.permit('pro')['pro'] == '1'
+      @members = Member.with_role(:professional).order(pseudo: :asc)
+      @title = 'PROFESSIONNELS'
+    else
+      @members = Member.without_role(:professional).order(pseudo: :asc)
+      @title = 'MEMBRES'
+    end
   end
 
   private
+
   def set_member
     @member = Member.find(params[:id])
   end
