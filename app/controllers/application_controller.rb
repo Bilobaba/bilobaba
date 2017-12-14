8615c707854967ff6592f286f20708f1abf12a45
Duy DANG <tao.zen.duy@gmail.com>
2017-12-14 16:56:22 +0100

    'Back' to the last page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,9 @@
+require 'pry'
+
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_devise_parameters, if: :devise_controller?
+  before_action :last_request
 
   def configure_devise_parameters
     devise_parameter_sanitizer.permit(:sign_up){|u| u.permit(:email, :password, :password_confirmation,
@@ -9,6 +12,13 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:account_update){|u| u.permit(:email, :password, :password_confirmation,
                                                               :pseudo, :first_name, :name,:bio, :birth_date,
                                                               :adress, :zip, :town, :country, :avatar, :current_password )}
+  end
+
+  def last_request
+    a = 1
+    # binding.pry
+    session[:request_back] ||= request.url
+    session[:request_back] = request.referer if (request.url != request.referer) &&  request.referer
   end
 end
 
