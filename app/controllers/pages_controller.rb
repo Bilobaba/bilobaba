2138fa90b13ebc9badecee279b72b454b3bf3765
Duy DANG <tao.zen.duy@gmail.com>
2017-12-24 03:38:53 +0100

    Test send mail heroku

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -17,8 +17,8 @@ class PagesController < ApplicationController
     @email = params[:message][:email]
     @message = params[:message][:message]
 
-    ContactMailer.contact(@author,@email,@message).deliver_now
     ContactMailer.contact_confirm(@author,@email,@message).deliver_now
+    ContactMailer.contact(@author,@email,@message).deliver_now
 
     flash[:notice] = "Votre message a bien été envoyé."
     redirect_to root_path
