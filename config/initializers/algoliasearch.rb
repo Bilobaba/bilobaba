8e23b9415f607c94859d28490645b8efd799c329
Duy DANG <tao.zen.duy@gmail.com>
2018-01-10 12:03:35 +0100

    fix var env algolia

--- config/initializers/algoliasearch.rb
+++ config/initializers/algoliasearch.rb
@@ -5,5 +5,5 @@ if Rails.env == "development"
   AlgoliaSearch.configuration = { application_id: 'OMM51UN985', api_key: 'e762415cd24e8a478a9dc52464d2ca5e' }
 else
   # works with account Algolia tao.zen.duy@gmail.com
-  AlgoliaSearch.configuration = { application_id: ENV['ALGOLIA_APPLICATION_ID'], api_key: ENV['ALGOLIA_ADMIN_API_KEY'] }
+  AlgoliaSearch.configuration = { application_id: ENV['ALGOLIASEARCH_APPLICATION_ID'], api_key: ENV['ALGOLIASEARCH_API_KEY'] }
 end
