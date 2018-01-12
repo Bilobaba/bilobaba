a3d86352196765a38081391f2b526dc181c82e6d
Duy DANG <tao.zen.duy@gmail.com>
2018-01-12 06:48:09 +0100

    fix algo key

--- config/initializers/algoliasearch.rb
+++ config/initializers/algoliasearch.rb
@@ -1,9 +1,5 @@
 # Keys depend on ENV
 
-if Rails.env == "development"
-  # works with account Algolia duy.dang@wanadoo.fr
-  AlgoliaSearch.configuration = { application_id: 'OMM51UN985', api_key: 'e762415cd24e8a478a9dc52464d2ca5e' }
-else
-  # works with account Algolia tao.zen.duy@gmail.com
-  AlgoliaSearch.configuration = { application_id: ENV['ALGOLIASEARCH_APPLICATION_ID'], api_key: ENV['ALGOLIASEARCH_API_KEY'] }
-end
+# PROD works with account Algolia tao.zen.duy@gmail.com
+# DEV works with account Algolia duy.dang@wanadoo.com
+AlgoliaSearch.configuration = { application_id: ENV['ALGOLIA_SEARCH_APPLICATION_ID'], api_key: ENV['ALGOLIA_SEARCH_API_ADMIN_KEY'] }
