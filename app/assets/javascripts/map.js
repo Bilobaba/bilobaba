52783b77f3aabc84f17bbf71f83fc98d6c63bdab
Kévin Arnoult <kevin.arnoult@gmail.com>
2018-02-02 16:13:37 +0100

    Move JS code to the appropriate JS file; Fill address, zip, and city fields

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -1,4 +1,4 @@
-var map;
+var autocomplete, map;
 
 function showMap(lat, lng) {
     latlng = new google.maps.LatLng({lat: lat, lng: lng}); 
@@ -22,4 +22,41 @@ function showMap(lat, lng) {
     });
 
     google.maps.event.trigger(map, "resize");
+}
+
+
+function initAutocomplete() {
+    if(typeof google == 'undefined' || !document.getElementById('address_autocomplete')) {
+        return;
+    }
+
+    var input = document.getElementById('address_autocomplete');
+    var options = {
+        bounds: new google.maps.LatLngBounds(
+            new google.maps.LatLng(48.6, 1.9),
+            new google.maps.LatLng(49.2, 2.9)
+        ),
+        componentRestrictions: {country: 'fr'}
+        //, types: ['establishment']
+    };
+
+    autocomplete = new google.maps.places.Autocomplete(input, options);
+    autocomplete.addListener('place_changed', fillInLatLng);
+}
+
+function fillInLatLng() {
+    var place = autocomplete.getPlace();
+    document.getElementById('event_lat').value = place.geometry.location.lat();
+    document.getElementById('event_lng').value = place.geometry.location.lng();
+    document.getElementById('event_address').value = place.name;
+    for (var i = 0; i < place.address_components.length; i++) {
+        switch (place.address_components[i].types[0]) {
+        case 'locality':
+            document.getElementById('event_city').value = place.address_components[i].long_name;
+            break;
+        case 'postal_code':
+            document.getElementById('event_zip').value = place.address_components[i].long_name;
+            break;
+        }
+    }
 }
