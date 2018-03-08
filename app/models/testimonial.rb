b1d549fab9dfe66368ea1a68c4b5786055e216f5
Duy DANG <tao.zen.duy@gmail.com>
2018-03-08 17:12:40 +0100

    testimonials v1

--- app/models/testimonial.rb
+++ app/models/testimonial.rb
@@ -1,9 +1,36 @@
 class Testimonial < ApplicationRecord
 
+  include Rails.application.routes.url_helpers
+  include AlgoliaSearch
+
   mount_uploader :image, ImageUploader
 
   belongs_to :member
 
   scope :published, -> { where(published: true) }
+
+  def member_name
+    self.member.name
+  end
+
+  def member_first_name
+    self.member.first_name
+  end
+
+  def member_avatar
+    return self.member.avatar.url
+  end
+
+  def member_pseudo
+    return self.member.pseudo
+  end
+
+  def member_bio
+    return self.member.bio
+  end
+
+  def url
+     return ENV["ROOT_URL"] + testimonial_path(self)
+  end
 
 end
