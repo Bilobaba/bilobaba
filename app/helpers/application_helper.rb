61c3033355e7b02ef36415ade9caef41905499c0
Duy DANG <tao.zen.duy@gmail.com>
2018-04-04 09:22:17 +0200

    gloogle map title in 2 lines

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -17,7 +17,8 @@ module ApplicationHelper
   # return the title for google map on 2 lines
   def map_title(title)
     tab = title.split(',')
-    map_title = tab[0..tab.size-3].join(',') + "\n" + tab[tab.size-2..tab.size-1].join(',')
-    return map_title
+    line_1 = tab[0..tab.size-3].join(',')
+    line_2 = tab[tab.size-2..tab.size-1].join(',')
+    return [line_1,line_2]
   end
 end
