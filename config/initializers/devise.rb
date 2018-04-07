d916b627fe2d65940b9dc4bedac692b17c2c4212
Duy DANG <tao.zen.duy@gmail.com>
2018-04-07 13:23:49 +0200

    Devise remember

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,3 +1,7 @@
+Devise::TRUE_VALUES << "on"
+Devise::TRUE_VALUES << "On"
+Devise::TRUE_VALUES << "ON"
+
 # Use this hook to configure devise mailer, warden hooks and so forth.
 # Many of these configuration options can be set straight in your model.
 Devise.setup do |config|
